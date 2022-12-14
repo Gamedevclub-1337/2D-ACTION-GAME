488141e9df5ae02db46f7572ef088ed7037bd2d6
Yassine Jerroudi <yjerroud@student.1337.ma>
2022-12-14 07:39:49 +0100

    remove the standrad libray from the readFileNullTerminated

--- src/tools.cpp
+++ src/tools.cpp
@@ -16,17 +16,40 @@ Buffer	readFile(const char *filepath)
 	fseek(file, 0, SEEK_SET);
 	result.mem = malloc(result.size);
 	
-	if (fread(result.mem, result.size, 1, file) == 0)
-	{
-		free(result.mem);
-		result.mem = NULL;
-		result.size = 0;
-		ERROR_LOG("fread(%s) Failed in : %s, %d\n", filepath, __FILE__, __LINE__);
-		return (result);
-	}
+	fread(result.mem, result.size, 1, file);
 	return (result);
 }
 
+// TODO(yassine): this is temporary
+
+Buffer	readFileNullTerminated(const char* filepath)
+{
+	Buffer	result = {};
+	
+	HANDLE	fileHandle = CreateFile(filepath,
+								   GENERIC_READ,
+								   FILE_SHARE_READ,
+								   NULL,
+								   OPEN_EXISTING,
+								   0, NULL);
+	result.size = GetFileSize(fileHandle, NULL);
+	result.mem = malloc(result.size + 1);
+	
+	DWORD	bytesRead = 0;
+	
+	if (!ReadFile(fileHandle, result.mem, result.size, &bytesRead, NULL) || bytesRead != result.size)
+	{
+		free(result.mem);
+		result.size = 0;
+		result.mem = NULL;
+		ERROR_LOG("fread(%s) Failed in : %s, %d\n", filepath, __FILE__, __LINE__);
+		return (result);
+	}
+	
+	return (result);
+}
+
+/*
 Buffer	readFileNullTerminated(const char* filepath)
 {
 	Buffer	result = {};
@@ -42,14 +65,8 @@ Buffer	readFileNullTerminated(const char* filepath)
 	fseek(file, 0, SEEK_SET);
 	result.mem = malloc(result.size + 1);
 	
-	if (fread(result.mem, result.size, 1, file) == 0)
-	{
-		free(result.mem);
-		result.mem = NULL;
-		result.size = 0;
-		ERROR_LOG("fread(%s) Failed in : %s, %d\n", filepath, __FILE__, __LINE__);
-		return (result);
-	}
+	fread(result.mem, result.size, 1, file);
 	((char*)result.mem)[result.size] = '\0';
 	return (result);
 }
+*/
